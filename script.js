f3e2e0d844ffad9ad9841adac018e54d76dcb6da
Viswabaddala16 <122138862+Viswabaddala16@users.noreply.github.com>
2023-10-21 09:20:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your code here
+document.addEventListener("DOMContentLoaded", () => {
+  let selectedImage = null;
+
+  const images = document.querySelectorAll(".image");
+  images.forEach((image) => {
+    image.addEventListener("dragstart", (e) => {
+      // Set the dragged image as the selected image
+      selectedImage = e.target;
+    });
+
+    image.addEventListener("dragend", () => {
+      // Clear the selected image
+      selectedImage = null;
+    });
+
+    image.addEventListener("dragover", (e) => {
+      e.preventDefault();
+    });
+
+    image.addEventListener("dragenter", (e) => {
+      e.preventDefault();
+      if (selectedImage !== null) {
+        // Swap the background images when dragging over
+        const tempImage = e.target.style.backgroundImage;
+        e.target.style.backgroundImage = selectedImage.style.backgroundImage;
+        selectedImage.style.backgroundImage = tempImage;
+      }
+    });
+  });
+});
+		
